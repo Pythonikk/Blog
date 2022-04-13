4cad46297032253491d55d9a1dac2e49d781c140
Pythonikk <nikkibrusoe@gmail.com>
2022-04-13 12:10:27 -0700

    add functionality to destroy a comment
    - add destroy action in comments controller
    - added link to destroy a comment in article show view
    - added dependent in article model to destroy comments when an article
    is destroyed
    - add basic authentication in articles controller

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,7 @@
 class Article < ApplicationRecord
   # adds the association with comment model
-  has_many :comments
+  # dependent: :destroy ensures that if an article is deleted, its comments will be deleted.
+  has_many :comments, dependent: :destroy
   # Active Record automatically defines model attributes for every table column,
   # so :title and :body don't need to be declared here.
   validates :title, presence: true
