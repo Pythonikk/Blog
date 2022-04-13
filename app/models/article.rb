003784353f09f37a8f15cf186b2a7cf55d549b22
Pythonikk <nikkibrusoe@gmail.com>
2022-04-13 09:19:40 -0700

    add create new article
    - add validations in article model
    - add error checks in new article view
    - add link to new article on index view

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,6 @@
 class Article < ApplicationRecord
+  # Active Record automatically defines model attributes for every table column,
+  # so :title and :body don't need to be declared here.
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
 end
